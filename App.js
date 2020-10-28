178c8a72f49f81c261e75f6fe68e3a113604208c
DESKTOP-95EUQVV\Suchinthana <such19@yahoo.com>
2020-10-28 20:35:05 +0530

    commit on 'master'

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import LoginScreen from './screens/login';
-import GalleryScreen from './screens/Gallery';
-import ForumScreen from './screens/Forum';
-import EventsScreen from './screens/Events';
-import NotificationScreen from './screens/Notification';
+import MainTabScreen from './navigation/MainTabNavigation';
 
-
-const MainTab = createMaterialBottomTabNavigator();
 const RootStack = createStackNavigator();
-
-function MainTabScreen() {
-  return (
-    <MainTab.Navigator
-    initialRouteName="Forum"
-    activeColor="#f0edf6"
-    inactiveColor="#3e2465"
-    barStyle={{ backgroundColor: '#694fad' }}
-    >
-      <MainTab.Screen name="Forum" component={ForumScreen} />
-      <MainTab.Screen name="Gallery" component={GalleryScreen} />
-      <MainTab.Screen name="Events" component={EventsScreen} />
-      <MainTab.Screen name="Notifications" component={NotificationScreen} />
-    </MainTab.Navigator>
-  );
-}
 
 export default function App() {
   return (
